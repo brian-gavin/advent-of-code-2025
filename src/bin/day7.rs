b1d82493569855d46a142c294d086525c92e559f
Brian Gavin <bgavin1996@gmail.com>
2025-12-07 13:39:07 -0800

    we did it reddit

--- src/bin/day7.rs
+++ src/bin/day7.rs
@@ -1,9 +1,10 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 use aoc::{
     Solution,
     grid::{Coord, Grid},
 };
+use itertools::Itertools;
 struct Day7;
 
 #[derive(Debug, Clone, Copy)]
@@ -26,11 +27,17 @@ impl From<char> for Cell {
     }
 }
 
+#[derive(Debug, Clone, Copy, Hash, PartialEq, Eq, PartialOrd, Ord)]
+enum Path {
+    Left,
+    Right,
+}
+
 impl Solution<usize> for Day7 {
     type Input = Grid<Cell>;
 
     fn parse_input() -> Self::Input {
-        Grid::from_input(include_str!("./7.sample.txt"))
+        Grid::from_input(include_str!("./7.txt"))
     }
 
     fn part1(mut grid: Self::Input) -> usize {
@@ -85,18 +92,19 @@ impl Solution<usize> for Day7 {
             }
         }
         fn fill_paths(
-            paths: &mut HashSet<(Coord, Coord)>,
+            paths: &mut HashMap<(Coord, Path), Coord>,
             grid: &Grid<Cell>,
             start: Coord,
+            path: Path,
             (end, is_splitter): (Coord, bool),
         ) {
-            if paths.contains(&(start, end)) {
+            if paths.contains_key(&(start, path)) {
                 return;
             }
-            paths.insert((start, end));
+            paths.insert((start, path), end);
             if is_splitter {
-                fill_paths(paths, grid, end, next_end(grid, end.west(1)));
-                fill_paths(paths, grid, end, next_end(grid, end.east(1)));
+                fill_paths(paths, grid, end, Path::Left, next_end(grid, end.west(1)));
+                fill_paths(paths, grid, end, Path::Right, next_end(grid, end.east(1)));
             }
         }
         let start = grid
@@ -104,20 +112,44 @@ impl Solution<usize> for Day7 {
             .find_map(|(coord, cell)| matches!(cell, Cell::Start).then_some(*coord))
             .unwrap();
         let first_split = next_end(&grid, start).0;
-        let mut paths = HashSet::new();
+        let mut paths = HashMap::new();
         fill_paths(
             &mut paths,
             &grid,
             first_split,
+            Path::Left,
             next_end(&grid, first_split.west(1)),
         );
         fill_paths(
             &mut paths,
             &grid,
             first_split,
+            Path::Right,
             next_end(&grid, first_split.east(1)),
         );
-        paths.len()
+        let mut splits = HashMap::new();
+        paths
+            .values()
+            .sorted()
+            .rev()
+            .cloned()
+            .chain(vec![first_split])
+            .for_each(|c| {
+                let left_end = paths.get(&(c, Path::Left));
+                let right_end = paths.get(&(c, Path::Right));
+                match (left_end, right_end) {
+                    (None, None) => {
+                        splits.insert(start, 0usize);
+                    }
+                    (Some(left_end), Some(right_end)) => {
+                        let left = splits.get(left_end).cloned().unwrap_or_default();
+                        let right = splits.get(right_end).cloned().unwrap_or_default();
+                        splits.insert(c, 1usize + left + right);
+                    }
+                    v => unreachable!("{:?}", v),
+                }
+            });
+        1 + *splits.get(&first_split).unwrap()
     }
 }
 
